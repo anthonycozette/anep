4c50f41b2765e9730261e1a7e0d494a663748b31
anthony <anthonycozette@hotmail.com>
2021-10-26 16:18:26 +0200

    formulaire

--- view/view.php
+++ view/view.php
@@ -1,37 +1,20 @@
-<!DOCTYPE html>
-<html>
+<?php
+while ($donnees = $req->fetch()) {
+?>
+    <div class="news">
+        <h3>
+            <?php echo htmlspecialchars($donnees['service']); ?>
+        </h3>
 
-<head>
-    <meta charset="utf-8" />
-    <title>Mon blog</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
-
-<body>
-    <h1>Mon super blog !</h1>
-    <p>Derniers billets du blog :</p>
-
-
-    <?php
-    while ($donnees = $req->fetch()) {
-    ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($donnees['service']); ?>
-            </h3>
-
-            <p>
-                <?php
-                echo nl2br(htmlspecialchars($donnees['service']));
-                ?>
-                <br />
-                <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-    <?php
-    }
-    $req->closeCursor();
-    ?>
-</body>
-
-</html>
+        <p>
+            <?php
+            echo nl2br(htmlspecialchars($donnees['service']));
+            ?>
+            <br />
+            <em><a href="#">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+}
+$req->closeCursor();
+?>
