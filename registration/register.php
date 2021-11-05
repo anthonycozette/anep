b8b5d27fbd0a63b197f8e76eefc7143019be7e89
anthony <anthonycozette@hotmail.com>
2021-11-05 12:15:50 +0100

    mise a jour connexion

--- registration/register.php
+++ registration/register.php
@@ -2,25 +2,22 @@
 <html>
 
 <head>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../styles/style.css" />
 </head>
 
 <body>
     <?php
     require('config.php');
-    if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])) {
+    if (isset($_REQUEST['username'], $_REQUEST['password'])) {
         // récupérer le nom d'utilisateur et supprimer les antislashes ajoutés par le formulaire
         $username = stripslashes($_REQUEST['username']);
         $username = mysqli_real_escape_string($conn, $username);
-        // récupérer l'email et supprimer les antislashes ajoutés par le formulaire
-        $email = stripslashes($_REQUEST['email']);
-        $email = mysqli_real_escape_string($conn, $email);
         // récupérer le mot de passe et supprimer les antislashes ajoutés par le formulaire
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($conn, $password);
         //requéte SQL + mot de passe crypté
-        $query = "INSERT into `users` (username, email, password)
-              VALUES ('$username', '$email', '" . hash('sha256', $password) . "')";
+        $query = "INSERT into `users` (username, password)
+              VALUES ('$username', '" . hash('sha256', $password) . "')";
         // Exécuter la requête sur la base de données
         $res = mysqli_query($conn, $query);
         if ($res) {
@@ -35,7 +32,6 @@
             
             <h1 class="box-title">S'inscrire</h1>
             <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
-            <input type="text" class="box-input" name="email" placeholder="Email" required />
             <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
             <input type="submit" name="submit" value="S'inscrire" class="box-button" />
             <p class="box-register">Déjà inscrit? <a href="login.php">Connectez-vous ici</a></p>
